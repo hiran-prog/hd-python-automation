8bd64b5f00cff6d6fe591d4635e94becfa88b4a5
Hiran Wajitha <hiranwajitha@outlook.com>
2026-05-24 22:02:06 +1000

    Variable exercise 03 done

---
+++ Learnings/Bootcamp/01_variables/var_Ex3.py
@@ -0,0 +1,7 @@
+# Exercise 3: Device Information Display
+
+device_name = "iPhone 16 Pro Max"
+firmware_version = "26.3.1"
+serial_number = "M944169684A"
+
+print(f"Device Name is {device_name}, running on firmware version {firmware_version} with serial number {serial_number}.")
