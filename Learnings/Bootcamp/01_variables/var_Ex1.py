b18734879f459f22461947d508934c31592d2505
Hiran Wajitha <hiranwajitha@outlook.com>
2026-05-24 17:17:59 +1000

    Variable exercise 01 done

---
+++ Learnings/Bootcamp/01_variables/var_Ex1.py
@@ -0,0 +1,6 @@
+# Exercise 1: Temperature Display
+
+temperature = 85 # in Celsius
+status = "Warning"
+
+print(f"{status}! Temperature is {temperature}°C")
---
+++ Learnings/Bootcamp/01_variables/var_Ex2.py
@@ -0,0 +1,7 @@
+# Exercise 2: Battery Percentage
+
+battery_percentage = 72 # in perentage
+charging_status = True
+
+print(f"Current Battery Percentage is: {battery_percentage}%")
+print(f"Charging status = {charging_status}")
