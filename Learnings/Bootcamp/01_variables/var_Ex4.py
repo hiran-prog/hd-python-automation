f67b05ab4e93b077036462604f61398c5969b732
Hiran Wajitha <hiranwajitha@outlook.com>
2026-05-27 14:19:04 +1000

    Variable exercise 04 on Aritmetics done

---
+++ Learnings/Bootcamp/01_variables/var_Ex4.py
@@ -0,0 +1,7 @@
+# Exercise 4: Arithmetic Operations
+
+voltage = 12 #in volts
+current = 3 # in amperes
+
+power = voltage * current #in watts
+print(f"Power consumed by the device is: {power} watts")
