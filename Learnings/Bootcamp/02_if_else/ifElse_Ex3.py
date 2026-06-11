ee37515d89ee2361e3d7595ad9147e420ddbb52c
Hiran Wajitha <hiranwajitha@outlook.com>
2026-06-11 21:14:08 +1000

    Conditions exercise 03 done

---
+++ Learnings/Bootcamp/02_if_else/ifElse_Ex3.py
@@ -0,0 +1,9 @@
+# Exercise 3: Login Simulation
+
+password = "python123"
+
+user_pw = input("Enter password: ")
+if user_pw == password:
+    print("Access granted!")
+else:
+    print("Access denied! Incorrect password.")
