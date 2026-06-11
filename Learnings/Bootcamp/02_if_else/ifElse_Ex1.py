9807092314446924ea9baaf1b95318fc69b1eb53
Hiran Wajitha <hiranwajitha@outlook.com>
2026-06-11 21:12:23 +1000

    Conditions exercise 01 done

---
+++ Learnings/Bootcamp/02_if_else/ifElse_Ex1.py
@@ -0,0 +1,11 @@
+# Exercise 1: Safe Temperature
+
+temp = int(input("Enter the temperature: "))
+
+if temp > 80:
+    print ("WARNING! Temperature is Hot.")
+else:
+    print("Temperature is Safe!")
+
+
+    
