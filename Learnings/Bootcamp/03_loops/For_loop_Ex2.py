5f552e8cbc40ce15ba2c2e310b7cbcf116917cf5
Hiran Wajitha <hiranwajitha@outlook.com>
2026-06-18 15:11:35 +1000

    For loop exercise 02 done

---
+++ Learnings/Bootcamp/03_loops/For_loop_Ex2.py
@@ -0,0 +1,6 @@
+# Exercise 2: Print all temperatures from list
+
+temperatures = [25, 30, 70, 14, 11, 36, 44, 8]
+
+for temp in temperatures:
+    print(f"The temperature values is: {temp} degrees Celsius")
